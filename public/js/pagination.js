c5c345fa3d5d1fb548b0d4f3e26205de3947ef06
kosmin <ksagwey.vlad@mail.ru>
2023-10-07 23:50:10 +0700

    UPDATE

--- public/js/pagination.js
+++ public/js/pagination.js
@@ -4,26 +4,29 @@ let page1 = document.querySelector("#separation1")
 let page2 = document.querySelector("#separation2")
 let page3 = document.querySelector("#separation3")
 let htmlNextPageBtn = document.querySelector(".nextPageBtn")
-let htmlDoGood = document.querySelector(".doGood")
+// let htmlDoGood = document.querySelector(".doGood")
 let body = document.querySelector(".body")
 let entryBlock = document.querySelector(".entry-block")
 let previousPageBtn = document.querySelector(".previousPageBtn")
 let weekDays = document.querySelector(".weekDays")
+let mainblock1 = document.querySelector(".main-block1")
+let genDate = document.querySelector("#gen-date")
 
 function nextPage() {
     if (pageNumber == 1) {
-        page1.classList.toggle("page-1")
-        page2.classList.toggle("page-2")
+        page1.classList.toggle("sep1")
+        page2.classList.toggle("sep2")
     }
     else if (pageNumber == 2) {
-        page2.classList.toggle("page-2")
-        page3.classList.toggle("page-3")
-        htmlNextPageBtn.classList.toggle("doGood")
-        htmlDoGood.classList.toggle("doGoodActive")
-        htmlDoGood.classList.toggle("doGood")
+        page2.classList.toggle("sep2")
+        page3.classList.toggle("sep3")
+        htmlNextPageBtn.classList.toggle("hide")
+        mainblock1.classList.toggle("auto")
+        htmlNextPageBtn.classList.toggle("zero-width")
     }
-    if (pageNumber<3) {
+    if (pageNumber < 3) {
         pageNumber += 1
+
     }
     htmlPageNum.innerHTML = `${pageNumber}`
     console.log(pageNumber);
@@ -31,15 +34,14 @@ function nextPage() {
 
 function previousPage() {
     if (pageNumber == 3) {
-        page3.classList.toggle("page-3")
-        page2.classList.toggle("page-2")
-        htmlNextPageBtn.classList.toggle("doGood")
-        htmlDoGood.classList.toggle("doGoodActive")
-        htmlDoGood.classList.toggle("doGood")
+        page3.classList.toggle("sep3")
+        page2.classList.toggle("sep2")
+        htmlNextPageBtn.classList.toggle("hide")
+        mainblock1.classList.toggle("auto")
     }
     else if (pageNumber == 2) {
-        page2.classList.toggle("page-2")
-        page1.classList.toggle("page-1")
+        page2.classList.toggle("sep2")
+        page1.classList.toggle("sep1")
     }
     if (pageNumber>1) {
         pageNumber -= 1
@@ -47,3 +49,4 @@ function previousPage() {
     htmlPageNum.innerHTML = `${pageNumber}`
     console.log(pageNumber);
 }
+
