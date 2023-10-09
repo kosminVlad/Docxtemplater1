d064bb5d61523074cf342e4297f04ae80dacba43
kosmin <ksagwey.vlad@mail.ru>
2023-10-09 17:39:24 +0700

    123

--- public/js/pagination.js
+++ public/js/pagination.js
@@ -4,11 +4,14 @@ let page1 = document.querySelector("#separation1")
 let page2 = document.querySelector("#separation2")
 let page3 = document.querySelector("#separation3")
 let htmlNextPageBtn = document.querySelector(".nextPageBtn")
-let htmlDoGood = document.querySelector(".doGood")
+// let htmlDoGood = document.querySelector(".doGood")
 let body = document.querySelector(".body")
 let entryBlock = document.querySelector(".entry-block")
 let previousPageBtn = document.querySelector(".previousPageBtn")
 let weekDays = document.querySelector(".weekDays")
+let mainblock1 = document.querySelector(".main-block1")
+let genDate = document.querySelector("#gen-date")
+let Page = document.querySelector(".Page")
 
 function nextPage() {
     if (pageNumber == 1) {
@@ -18,12 +21,14 @@ function nextPage() {
     else if (pageNumber == 2) {
         page2.classList.toggle("sep2")
         page3.classList.toggle("sep3")
-        htmlNextPageBtn.classList.toggle("doGood")
-        htmlDoGood.classList.toggle("doGoodActive")
-        htmlDoGood.classList.toggle("doGood")
+        htmlNextPageBtn.classList.toggle("hide")
+        mainblock1.classList.toggle("auto")
+        htmlNextPageBtn.classList.toggle("zero-width")
+        Page.classList.toggle("mrgn-top")
     }
     if (pageNumber < 3) {
         pageNumber += 1
+
     }
     htmlPageNum.innerHTML = `${pageNumber}`
     console.log(pageNumber);
@@ -33,9 +38,8 @@ function previousPage() {
     if (pageNumber == 3) {
         page3.classList.toggle("sep3")
         page2.classList.toggle("sep2")
-        htmlNextPageBtn.classList.toggle("doGood")
-        htmlDoGood.classList.toggle("doGoodActive")
-        htmlDoGood.classList.toggle("doGood")
+        htmlNextPageBtn.classList.toggle("hide")
+        mainblock1.classList.toggle("auto")
     }
     else if (pageNumber == 2) {
         page2.classList.toggle("sep2")
@@ -47,3 +51,15 @@ function previousPage() {
     htmlPageNum.innerHTML = `${pageNumber}`
     console.log(pageNumber);
 }
+function start(){    window.onkeydown = keyprogram
+}
+function keyprogram(e){ if (e.keyCode == 37){
+    previousPage()
+    }
+    else if(e.keyCode == 	39){
+        nextPage()
+    }
+}
+
+window.onload = start
+
--- server.js
+++ server.js
@@ -8,89 +8,58 @@ const path = require("path");
 
 const app = express();
 
-// настройка middleware для обработки данных формы
+// Middleware setup
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-// serve static files from the public directory
 app.use(express.static("public"));
 
-// // обработчик GET запроса на получение главной страницы
-// app.get("/", (req, res) => {
-//   res.sendFile(__dirname + "/index.html");
-// });
+// Routes
+app.post("/generate", generateDocument);
 
-// обработчик POST запроса на генерацию документа
-app.post("/generate", (req, res) => {
-  // загрузка содержимого файла шаблона
-  const templatePath = path.join(__dirname, "template.docx");
-  console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-  console.log(templatePath);
-  console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+// Server setup
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
 
-  const templateContent = fs.readFileSync(templatePath, "binary");
+// Route handlers
+function generateDocument(req, res) {
+  const data = extractDataFromRequest(req);
+  const templateContent = loadTemplate(data.hours);
+  const doc = createDocxtemplater(templateContent);
 
-  // создание объекта Docxtemplater
-  const doc = new Docxtemplater();
-  doc.loadZip(new JSZip(templateContent));
-
-  // получение данных из формы и передача их в шаблон
-  // const data = {
-  //   name: req.body.name,
-  //   kvant_name: req.body.kvant_name,
-  //   year: req.body.year,
-  //   group: req.body.group,
-  //   module: req.body.module,
-  //   doc_name: req.body.doc_name,
-  //   head_1: req.body.head_1,
-  //   head_2: req.body.head_2,
-  //   head_3: req.body.head_3,
-  //   head_4: req.body.head_4,
-  //   head_5: req.body.head_5,
-  //   date_1: req.body.date_1,
-  //   date_2: req.body.date_2,
-  //   date_3: req.body.date_3,
-  //   date_4: req.body.date_4,
-  //   date_5: req.body.date_5,
-  // };
-
-  const { name, kvant_name, year, group, module, doc_name, ...dates } = req.body;
-
-const data = {
-  name,
-  kvant_name,
-  year,
-  group,
-  module,
-  doc_name,
-  ...dates
-};
-
-
-  console.log(data);
-
-  doc.setData(data);
-
-  // генерация нового документа на основе шаблона и данных
   try {
+    doc.setData(data);
     doc.render();
   } catch (error) {
-    res.status(500).send("Ошибка при генерации документа: " + error);
+    res.status(500).send("Error generating document: " + error);
     return;
   }
 
-  // получение содержимого сгенерированного документа
   const generatedContent = doc.getZip().generate({ type: "nodebuffer" });
+  sendGeneratedDocument(res, generatedContent);
+}
 
-  // отправка сгенерированного документа в виде вложения
+// Helper functions
+function loadTemplate(hours) {
+
+  const templatePath = path.join(__dirname, `template_${hours}.docx`);    
+
+  return fs.readFileSync(templatePath, "binary");
+}
+
+function createDocxtemplater(templateContent) {
+  const doc = new Docxtemplater();
+  doc.loadZip(new JSZip(templateContent));
+  return doc;
+}
+
+function extractDataFromRequest(req) {
+  const { name, kvant_name, year, group, module, doc_name, ...dates } = req.body;
+  return { name, kvant_name, year, group, module, doc_name, ...dates };
+}
+
+function sendGeneratedDocument(res, generatedContent) {
   res.set("Content-Disposition", "attachment; filename=generated.docx");
   res.set("Content-Type", "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
   res.send(generatedContent);
-});
-
-// запуск сервера
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Сервер запущен на порту ${port}`);
-});
+}
